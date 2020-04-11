a77a447c82e687bc0248acc117cfd7e5bd5273a8
WEYAI <840274774@qq.com>
2020-04-11 22:26:34 +0800

    add count the sentence of the contoNotes5.0

--- TASK/utils/split_conll2000_dev.py
+++ TASK/utils/split_conll2000_dev.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
-def split_conll():
+from sklearn.model_selection import train_test_split
+def split_conll(src,target):
+    with open(src, 'r+', encoding='utf-8') as file:
+        # for line in file:
+        #     line = file.readlines()++----------
+        #     line_len = len(line)
+        #     if line_len == 0:
+        #         import random
+        #         data = file.read().split('\n')
+        #         random.shuffle(data)
+        #         train_data = data[:50]
+        #         test_data = data[50:]
+        #         print(train_data)
+        #         print(test_data)
+
+        xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.2, random_state=0)
+
+        print("test_data........"+xTrain)
+if __name__ == '__main__':
+    split_conll("/home/zutnlp/wueryong/projects/corpus/conll2000/train.txt","");
---
+++ msra-chinese-word-segmentation-data-v1/count_sentences.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# @Time    : 4/11/20 10:24 PM
+# @Author  : WEY
+# @File    : count_sentences
+# @Software: PyCharm
+# -*- coding: utf-8 -*-
+# @Time    : 4/11/20 5:00 PM
+# @Author  : WEY
+# @File    : count_sentences
+# @Software: PyCharm
+
+def count_sen(path):
+    with open(path, 'r+', encoding='utf-8') as file:
+        i = -1
+        tag = 0
+        line: str
+        for line in file:
+            if '\n' == line and tag == 0:
+                i = i + 1
+                tag = 1
+            else:
+                tag = 0
+        print(path, i)
+
+
+if __name__ == '__main__':
+    path1 = '/home/zutnlp/corpus/notoNotes5.0/onto.train.ner'
+    path2 = '/home/zutnlp/corpus/notoNotes5.0/onto.development.ner'
+    path3 = '/home/zutnlp/corpus/notoNotes5.0/onto.test.ner'
+    count_sen(path1)
+    count_sen(path2)
+    count_sen(path3)
