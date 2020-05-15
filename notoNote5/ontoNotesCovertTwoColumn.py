3e175860c792129ea8847ca6d7fedca7e037900a
WEYAI <840274774@qq.com>
2020-05-15 08:03:55 +0800

    format the OntonotesconvertTwoColumn

--- notoNote5/ontoNotesCovertTwoColumn.py
+++ notoNote5/ontoNotesCovertTwoColumn.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # @File     : ontonoes
-# @Author   : 张志毅
 # @Time     : 2020/4/12 11:25
-with open('/home/zutnlp/corpus/CoNLL-2003/eng.train', 'r',
-          encoding='utf-8') as f:
-    lines = f.readlines()
-    for line in lines:
-        temp = line.split()
-        with open('/home/zutnlp/corpus/CoNLL-2003/eng.ner.train', 'a', encoding='utf-8') as af:
-            if len(temp) != 0:
-                af.write('{} {}\n'.format(temp[0], temp[3]))
-            else:
-                af.write('\n')
-                # print(temp[0])
-            # print('\n')
+import os
+
+
+def convert(source, target, dir):
+    '''
+
+    :param source:the source  file path
+    :param target:the result file path
+    :param dir: if the path don't exit create the directory
+    :return:
+    '''
+    with open(source, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            temp = line.split()
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+            with open(target, 'a', encoding='utf-8') as af:
+                if len(temp) != 0:
+                    af.write('{} {}\n'.format(temp[0], temp[3]))
+                else:
+                    af.write('\n')
+                    # print(temp[0])
+                # print('\n')
+
+
+if __name__ == '__main__':
+    convert('/home/zutnlp/corpus/notoNotes5.0/onto.development.ner',
+            '/home/zutnlp/corpus/notoNotes5.0/ner/onto.development.ner', '/home/zutnlp/corpus/notoNotes5.0/ner')
+    convert('/home/zutnlp/corpus/notoNotes5.0/onto.train.ner', '/home/zutnlp/corpus/notoNotes5.0/ner/onto.train.ner',
+            '/home/zutnlp/corpus/notoNotes5.0/ner')
+    convert('/home/zutnlp/corpus/notoNotes5.0/onto.test.ner', '/home/zutnlp/corpus/notoNotes5.0/ner/onto.test.ner',
+            '/home/zutnlp/corpus/notoNotes5.0/ner')
